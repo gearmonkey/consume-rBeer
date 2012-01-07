0db51e028f9252efce95ec6c32f83c37115eda4c
Ben Fields <me@benfields.net>
2012-01-07 13:18:07 +0000

    a simple setup.py;mostly a placeholder

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='consume-rBeer',
+      version='0.1',
+      description='Eating ratebeer for breakfast',
+      author='Ben Fields',
+      author_email='me@benfields.net',
+      py_modules=['rbeer'],
+     )
