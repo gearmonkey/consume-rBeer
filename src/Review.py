9cc0e764804883bc5cefeb0aabbbd47600722bd1
Ben Fields <me@benfields.net>
2012-01-07 14:11:27 +0000

    the review itself clearly should be a class

---
+++ src/Review.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Review.py
+
+Created by Benjamin Fields on 2012-01-07.
+Copyright (c) 2012 . All rights reserved.
+"""
+
+import sys
+import os
+import unittest
+
+
+class Review:
+	def __init__(self):
+		pass
+
+
+class ReviewTests(unittest.TestCase):
+	def setUp(self):
+		pass
+
+
+if __name__ == '__main__':
+	unittest.main()
