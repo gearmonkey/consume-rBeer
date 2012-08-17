20a423ca55561d22125a8459f2a043a7305783ab
Ben Fields <me@benfields.net>
2012-08-17 12:44:54 -0400

    make an idf model, optionally in pieces and sort the idf

---
+++ recs/build_idf.py
@@ -0,0 +1,74 @@
+import sqlite3
+import logging
+import sys
+
+from tfidf import TfIdf
+
+
+conn = sqlite3.connect('../rbeer.db')
+c = conn.cursor()
+
+try:
+    bottom_idx = sys.argv[1]
+    print "using", bottom_idx, "as start point"
+except IndexError:
+    bottom_idx = None
+try:
+    top_idx = sys.argv[2]
+    print "using", top_idx, "as end point"
+except IndexError:
+    top_idx = None
+
+comment_model = TfIdf(stopword_filename="stopwords.txt", 
+                        DEFAULT_IDF=None)
+if bottom_idx or top_idx:
+    suffix = '_'
+    if bottom_idx:
+        suffix += str(bottom_idx)
+    else:
+        suffix += "0"
+    if top_idx:
+        suffix += "-"+str(top_idx)
+else:
+    suffix = ""
+idf_model_save_path = "idf_model{0}.txt".format(suffix)
+inferred_stopwords_path = "inferred_stopwords{0}.txt".format(suffix)
+#find the number of beers for progress indication
+c.execute("SELECT id from beer")
+total_beers = len(list(c.fetchall()))
+print "idf will be built from the reviews of ", total_beers, "beers."
+
+c.execute("SELECT id, brewery FROM beer")
+idx = 0 #don't want to unwrap the generator so we'll idex this way
+worked = 0
+for beer_id, name in c.fetchall():
+    if bottom_idx and idx<bottom_idx:
+        idx+=1
+        continue
+    if top_idx and idx>top_idx:
+        break
+    if idx%1000 == 0:
+        print """*-*-*-* Finished {0}% of the processing.""".format(float(idx)/total_beers)
+        comment_model.save_corpus_to_file(idf_model_save_path,inferred_stopwords_path)
+    idx += 1
+    try:
+        int(beer_id)
+    except:
+        print "the beer", name, "has this id", beer_id, "which doesn't look like an int..."
+        continue
+    try:
+        c.execute("SELECT comment FROM review WHERE beer_uid = {0}".format(beer_id))
+    except Exception, err:
+        print "the beer", name, "has this id", beer_id, "and is giving this error:", err
+        continue
+    comments = ''
+    for comment, in c.fetchall():
+        comments += comment
+    comment_model.add_input_document(comments)
+    print "dealt with", name
+    worked += 1
+print "sucessfully dealt with", worked, "beers."
+print "finished, storing model..."
+comment_model.save_corpus_to_file(idf_model_save_path,inferred_stopwords_path)
+
+    
---
+++ recs/sortidf.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+sortidf.py
+
+pulls in an idf model file, that looks like:
+
+[number of documents]
+[term1]:[document count]
+[term2]:[document count]
+  ...
+[termN]:[document count]
+
+and sorts the term list according to the document count, then writes it back to disk.
+
+Usage:
+
+> sortidf.py infile outfile
+
+Created by Benjamin Fields on 2012-08-16.
+Copyright (c) 2012 . All rights reserved.
+"""
+
+import sys
+import codecs
+from operator import itemgetter
+
+
+help_message = '''
+Usage:
+> sortidf.py infile outfile
+'''
+
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg + '\n' + help_message
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        infile, outfile = argv[1:]
+    except getopt.error, msg:
+        raise Usage(msg)
+    with codecs.open(infile, 'r', encoding='utf-8') as rh:
+        term_counts = [(line.split(':')[0].strip(), int(line.split(':')[1].strip())) for line in rh.readlines()\
+                            if len(line.split(':')) == 2]
+        rh.seek(0)
+        num_terms = int(rh.readline().strip())
+    print 'read in', len(term_counts), 'terms from', num_terms, 'documents.'
+    term_counts.sort(key=itemgetter(1), reverse=True)
+    with codecs.open(outfile, 'wb', encoding='utf-8') as wh:
+        wh.write(str(num_terms)+'\n')
+        wh.write(u'\n'.join([u'{0}:{1}'.format(term, count) for term, count in term_counts]))
+        wh.write('\n')
+
+    
+
+
+
+if __name__ == "__main__":
+    sys.exit(main())
