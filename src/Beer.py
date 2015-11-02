c0cf72276457a7184def2fddbedbffc8d3aa95e4
Ben Fields <me@benfields.net>
2015-11-02 15:51:03 +0000

    tweaking the scraper regex after the RB UI update

--- src/Beer.py
+++ src/Beer.py
@@ -24,16 +24,16 @@ class Beer(object):
     beer_uri = "http://www.ratebeer.com/beer/beer_name/{beer_id}/"
     ratings_uri = "http://www.ratebeer.com/Ratings-Who.asp?BeerID={beer_id}"
     ratings_regex = re.compile(r'<a href="/ViewUser\.asp\?UserID=(?P<userid>\d*)" target="_blank">(.*?)</a> \(<i><a href="/beer/beer_name/(\d*)/(?P=userid)/" target="_blank">Rating - (.*?)</a></i>\)')
-    
-    reviews_regex = rev = re.compile(r'<div style="display:inline; padding: 0px 0px; font-size: 24px; font-weight: bold; color: #036;" title="(?P<rating>\d\.?\d?) out of 5\.0">(?P=rating)</div><strong>&nbsp;&nbsp;<small> AROMA </small><big style="color: #999;">(\d\d?)/10</big>&nbsp;&nbsp;<small> APPEARANCE </small><big style="color: #999;">(\d)/5</big>&nbsp;&nbsp;<small> TASTE </small><big style="color: #999;">(\d\d?)/10</big>&nbsp;&nbsp;<small> PALATE </small><big style="color: #999;">(\d)/5</big>&nbsp;&nbsp;<small> OVERALL </small><big style="color: #999;">(\d\d?)/20</big></strong></div><small style="color: #666666; font-size: 12px; font-weight: bold;"><A HREF="/user/(\d*?)/">(\S*?)&nbsp;\(\d*?\)</A></I> - (.*?) - ([A-Z]{3} \d\d?, \d{4}).*?</small><BR><div style="padding: 20px 10px 20px 0px; border-bottom: 1px solid #e0e0e0; line-height: 1\.5;">(.*?)</div><br>', flags=re.DOTALL)
-    
-    get_metadata = {'name': re.compile(r'<div class="user-header"><h1>(.*?)</h1></div>'),
+
+    reviews_regex = re.compile(r'<div style="display:inline; padding: 0px 0px; font-size: 24px; font-weight: bold; color: #036;" title="(?P<rating>\d\.?\d?) out of 5\.0">(?P=rating)</div><strong>&nbsp;&nbsp;<small> AROMA </small><big style="color: #999;">(\d\d?)/10</big>&nbsp;&nbsp;<small> APPEARANCE </small><big style="color: #999;">(\d)/5</big>&nbsp;&nbsp;<small> TASTE </small><big style="color: #999;">(\d\d?)/10</big>&nbsp;&nbsp;<small> PALATE </small><big style="color: #999;">(\d)/5</big>&nbsp;&nbsp;<small> OVERALL </small><big style="color: #999;">(\d\d?)/20</big></strong></div><small style="color: #666666; font-size: 12px; font-weight: bold;"><A HREF="/user/(\d*?)/">(\S*?)&nbsp;\(\d*?\)</A></I> - (.*?) - ([A-Z]{3} \d\d?, \d{4}).*?</small><BR><div style="padding: 20px 10px 20px 0px; border-bottom: 1px solid #e0e0e0; line-height: 1\.5;">(.*?)</div><br>', flags=re.DOTALL)
+
+    get_metadata = {'name': re.compile(r'<div class="user-header"><h1.*>(.*?)</h1></div>'),
                     'abv': re.compile(r'<abbr title="Alcohol By Volume">ABV</abbr>: <big style="color: #777;"><strong>(.*?)%</strong></big>'),
                     'mean': re.compile(r'MEAN: <big style="color: #777;"><strong>(\d\.?\d?)/5\.0</strong></big>') ,
-                    'weighted_score': re.compile(r'<a name="real average" title="The weighted average, (\d\.?\d*?), is the Bayesian mean of all qualified ratings for this beer" >WEIGHTED AVG: <big style="color: #777;"><strong>\d\.?\d*?</strong></big></a>'),
-                    'overall_percentile': re.compile(r'<td align=center colspan=2 style="background-color: #036; font-size: 72px; font-weight: bold; color: #fff; padding: 10px;" title="(\d?\d?\d\.?\d*?): This figure represents a beer\x92s performance relative to all beers on an adjusted percentile basis\. Find out more in the FAQ\."'),
-                    'style_percentile': re.compile(r'<td align=center style="background-color: #66A212; font-size: 28px; font-weight: bold; color: #fff; padding: 10px;" colspan=2 title="(\d?\d?\d\.?\d*?): This figure represents a beer\x92s performance relative to its peers in the same beer style category on an adjusted percentile basis. Find out more in the FAQ\.">'),
-                    'total_ratings': re.compile(r'RATINGS: </abbr><big style="color: #777;"><b>(\d+)</b></big>'),
+                    'weighted_score': re.compile(r'<a name="real average" title="The weighted average, (\d\.?\d*?), is the Bayesian mean of all qualified ratings for this beer" >WEIGHTED AVG: <big style="color: #777;"><strong><span itemprop="ratingValue">\d\.?\d*?</span>/<span itemprop="bestRating">5</span></strong></big></a>'),
+                    'overall_percentile': re.compile(r'<div style="font-size: 48px; font-weight: bold; color: #fff; padding: 7px 10px;" title="(\d?\d?\d\.?\d*?): This figure represents a beer\x92s performance relative to all beers on an adjusted percentile basis. Find out more in the FAQ.">'),
+                    'style_percentile': re.compile(r'<div style="background-color: #66A212; position: relative; left: 0px; top: -20px; width: 67px; height: 67px; border-radius: 67px; z-index: -1;text-align: center;" title="(\d?\d?\d\.?\d*?): This figure represents a beer\x92s performance relative to its peers in the same beer style category on an adjusted percentile basis. Find out more in the FAQ.">'),
+                    'total_ratings': re.compile(r'RATINGS: </abbr><big style="color: #777;"><b><span itemprop="reviewCount">(\d+)</span>'),
                     'brewery_and_style': re.compile(r'<div style="padding-bottom: 7px; line-height: 1.5;"><big>Brewed by <b><a href="/brewers/.*?/(\d+)/">.*?</A></b></big><br>Style: <a href="/beerstyles/.*?/(\d+)/">.*?</a><br>.*?</div>')}
     def __init__(self, beer_uid, name=None, abv=None, mean_score=None, overall_percentile=None,
                 style_percentile=None, total_ratings=None, brewery_id=None):
@@ -53,7 +53,7 @@ class Beer(object):
             self.brewery_id=int(brewery_id)
     def __str__(self):
         return "'{0}' from {1}".format(self.name, Beer.beer_uri.format(beer_id=self.uid))
-    
+
     def fetch_beer_page(self, page=None):
         """
         returns html string of the beer page
@@ -64,11 +64,11 @@ class Beer(object):
         else:
             logging.debug('opening ' + Beer.beer_uri.format(beer_id=self.uid))
             return urllib2.urlopen(Beer.beer_uri.format(beer_id=self.uid)).read()
-    
+
     def parse_metadata(self, raw_page=None):
         """
         scrapes the beer metadata out of a beer's raw_page string
-        Note! overrides existing metadata, 
+        Note! overrides existing metadata,
             catches non-existance for abv and mean score as these aren't on every beer page
         """
         if not raw_page:
@@ -111,21 +111,21 @@ class Beer(object):
         returns the html string of the minimized rating list
         """
         return urllib2.urlopen(Beer.ratings_uri.format(beer_id=self.uid)).read()
-        
+
     def scrape_user_rating_list(self, raw_page=None):
         """
-        scrapes the minimized user comment list (basically just ratings), 
+        scrapes the minimized user comment list (basically just ratings),
         creating a list of tuples in self.ratings where each tuple takes the form
         (username, userID, beerRating)
         username is a str, userID an int, and beerRating a float
-        the prime advantage of this over scrape_user_comment_list is that it 
+        the prime advantage of this over scrape_user_comment_list is that it
         takes only one http GET to fetch this entire list, while gathering full
-        comments and ratings by beer takes N/10 GETs where N is the number of 
+        comments and ratings by beer takes N/10 GETs where N is the number of
         comments (though there may be undocumented means of increasing the page size)
         """
         if not raw_page:
             raw_page = self.fetch_rating_page()
-        self.ratings = [(username, int(userID), float(beerRating)) 
+        self.ratings = [(username, int(userID), float(beerRating))
                         for (userID, username, beerID, beerRating) in Beer.ratings_regex.findall(raw_page)]
 
     def scrape_user_comment_list(self, raw_page=None):
@@ -146,15 +146,15 @@ class Beer(object):
                                     taste_score=int(taste), palete_score=int(palete),
                                     overall_score=int(overall), user_loc=user_loc,
                                     date = datetime.datetime.strptime(date_str, '%b %d, %Y').date(),
-                                    comment = comment) for (topline_score, aroma, apperance, 
-                                                        taste, palete, overall, user_id, user_name, user_loc, 
+                                    comment = comment) for (topline_score, aroma, apperance,
+                                                        taste, palete, overall, user_id, user_name, user_loc,
                                                         date_str, comment) in \
                                                                 Beer.reviews_regex.findall(raw_page)]
             page += 1
             if page-1  > self.total_ratings/8.0:
                 logging.error('parsing should have completed, but did not, forcing.')
                 break
-        
+
 class BeerTests(unittest.TestCase):
     def setUp(self):
         pass
